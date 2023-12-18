ad62d10aeafb0f2511ee1de8cfabc14ac9d2d233
TheR3dWizard <sreeraghavan2016@gmail.com>
2023-12-18 18:00:12 +0530

    added menu

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 import Employee.EmployeeData;
 import Database.ConnectionHelper;
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws ClassNotFoundException {
         Class.forName("com.mysql.cj.jdbc.Driver");
@@ -9,5 +9,45 @@ public class Main {
         System.out.println();    
         String newQuery = "SELECT * FROM Employee";
         ConnectionHelper.selectQuery(newQuery);
+        
+        // Create a menu to access all the functions in the ConnectionHelper class
+        Scanner scanner = new Scanner(System.in);
+        int choice;
+        do {
+            System.out.println("Menu:");
+            System.out.println("1. Reset all tables");
+            System.out.println("2. Create employee from user input and store");
+            System.out.println("3. Select query");
+            System.out.println("4. Exit");
+            System.out.print("Enter your choice: ");
+            choice = scanner.nextInt();
+            
+            switch (choice) {
+                case 1:
+                    ConnectionHelper.resetAllTables();
+                    break;
+                case 2:
+                    ConnectionHelper.createEmployeeFromUserInputandStore();
+                    break;
+                case 3:
+                    System.out.print("Enter the query: ");
+                    scanner.nextLine(); // Consume the newline character
+                    String query = scanner.nextLine();
+                    ConnectionHelper.selectQuery(query);
+                    break;
+                case 4:
+                    System.out.println("Exiting...");
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+                    break;
+            }
+            
+            System.out.println();
+        } while (choice != 4);
+        
+        scanner.close();
     }
 }
+
+
